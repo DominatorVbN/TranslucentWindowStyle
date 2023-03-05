cc70b88595075749189ecf39e4b3255bbeab1625
Amit Samant <amit.samant@gojek.com>
2023-03-05 16:40:32 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TranslucentWindowStyle
+
+A description of this package.
---
+++ Sources/TranslucentWindowStyle/TranslucentWindowStyle.swift
@@ -0,0 +1,6 @@
+public struct TranslucentWindowStyle {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
